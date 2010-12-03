7420824a4b13cd91fa7518a78b91ec0a155b7d30
matti.annala@tut.fi <matti.annala@tut.fi@3a7f2580-edfc-f23b-ebb8-262a95d2b863>
2010-12-03 11:31:51 +0000

    There's too many human SNPs. Split the SNP data structures per-chromosome.

--- trunk/pipeline/import/dbsnp/read_dbsnp.m
+++ trunk/pipeline/import/dbsnp/read_dbsnp.m
@@ -1,8 +1,4 @@
-function dbsnp = read_dbsnp()
-
-global organism;
-
-chr_snps = repmat({struct}, length(organism.Chromosomes.Name), 1);
+function [] = read_dbsnp()
 
 files = dir();
 for k = 1:length(files)
@@ -12,11 +8,11 @@ for k = 1:length(files)
 	token = tokens{1}; chr = token{1};
 	filepath = files(k).name;
 	
+	fprintf(1, 'Reading chromosomes from file %s...\n', filepath);
 	chr = chromosome_sym2num(chr);
-	chr_snps{chr} = parse_dbsnp_chromosome(filepath, chr);
+	parse_dbsnp_chromosome(filepath, chr);
 end
 
-dbsnp = cat_structs(chr_snps{:});
 
 
 
@@ -24,67 +20,73 @@ dbsnp = cat_structs(chr_snps{:});
 
 
 
-function snp = parse_dbsnp_chromosome(filepath, chr)
+function [] = parse_dbsnp_chromosome(filepath, chr)
 
-snp_prealloc = 100000;
+global organism;
 
-snp = struct;
-snp.ID = cell(snp_prealloc, 1);
-snp.Validated = false(snp_prealloc, 1);
-snp.Chromosome = chr * ones(snp_prealloc, 1);
-snp.Offset = zeros(snp_prealloc, 1);
-snp.Alleles = cell(snp_prealloc, 1);
+snp_prealloc = 10^7;
+
+snps = struct;
+snps.ID = zeros(snp_prealloc, 1);
+snps.Validated = false(snp_prealloc, 1);
+snps.Offset = zeros(snp_prealloc, 1);
+snps.Alleles = cell(snp_prealloc, 1);
 
 N = 0;
 
 fid = fopen(filepath);
+
 while 1
 	line = fgetl(fid);
 	if line == -1, break, end
+		
+	if length(line) < 4 || strcmp(line(1:2), 'ss'), continue, end
 	
-	tokens = regexpi(line, '^(rs\d+) \| human', 'tokens');
-	if length(tokens) == 1
-		token = tokens{1}; id = token{1};
-		N = N + 1;
-		snp.ID{N} = id;
+	if strcmp(line(1:2), 'rs')
+		tokens = regexpi(line, '^rs(\d+) \|', 'tokens');
+		if length(tokens) == 1
+			token = tokens{1}; id = str2double(token{1});
+			N = N + 1;
+			snps.ID(N) = id;
+		end
 		continue;
 	end
 	
-	tokens = regexpi(line, '^SNP \| alleles=''(.+)'' \|', 'tokens');
-	if length(tokens) == 1
-		token = tokens{1}; alleles = token{1};
-		alleles = strrep(alleles, '/', '');
-		snp.Alleles{N} = alleles;
+	code = line(1:3);
+	
+	if strcmp(code, 'SNP')
+		tokens = regexpi(line, '^SNP \| alleles=''(.+)'' \|', 'tokens');
+		if length(tokens) == 1
+			token = tokens{1}; alleles = token{1};
+			snps.Alleles{N} = alleles;
+		end
+		continue;
+	end
+	
+	if strcmp(code, 'CTG')
+		tokens = regexpi(line, '^CTG \| assembly=GRCh37 .* \| chr-pos=(\d+)',...
+			'tokens');
+		if length(tokens) == 1
+			token = tokens{1};
+			snps.Offset(N) = str2double(token{1});
+		end
 		continue;
 	end
 	
 	if regexpi(line, '^VAL \| validated=YES')
-		snp.Validated(N) = true;
-		continue;
-	end
-	
-	tokens = regexpi(line, ...
-		'^CTG \| .* \| chr-pos=(\d+) \| .* \| orient=(.)', 'tokens');
-	if length(tokens) == 1
-		token = tokens{1};
-		offset = str2double(token{1}); strand = token{2};
-		snp.Offset(N) = offset;
-		if strcmp(strand, '-')
-			snp.Alleles{N} = seqcomplement(snp.Alleles{N});
-		elseif ~strcmp(strand, '+')
-			error 'Unsupported strand.';
-		end
+		snps.Validated(N) = true;
 		continue;
 	end
 end
 
 fclose(fid);
 
-[~, order] = sort(snp.Offset(1:N));
+[~, order] = sort(snps.Offset(1:N));
 
-snp.ID = snp.ID(order);
-snp.Validated = snp.Validated(order);
-snp.Chromosome = snp.Chromosome(order);
-snp.Offset = snp.Offset(order);
-snp.Alleles = snp.Alleles(order);
+snps.ID = snps.ID(order);
+snps.Validated = snps.Validated(order);
+snps.Offset = snps.Offset(order);
+snps.Alleles = snps.Alleles(order);
+
+save(sprintf('chr%s', organism.Chromosomes.Name{chr}), 'snps');
 
