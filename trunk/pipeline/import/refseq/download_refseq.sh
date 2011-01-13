376c681a7d7a43eb6ae6c8c7fe8ce9a1509865c8
matti.annala@tut.fi <matti.annala@tut.fi@3a7f2580-edfc-f23b-ebb8-262a95d2b863>
2011-01-13 13:15:53 +0000

    Massively improved RefSeq transcriptome build parsing. More information is now retrieved, and exons in particular are stored in a more useful way.

--- trunk/pipeline/import/refseq/download_refseq.sh
+++ trunk/pipeline/import/refseq/download_refseq.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-for num in `seq 56`; do wget -nc ftp://ftp.ncbi.nih.gov/refseq/release/vertebrate_mammalian/vertebrate_mammalian$num.rna.gbff.gz; done
-for num in `seq 56`; do wget -nc ftp://ftp.ncbi.nih.gov/refseq/release/vertebrate_mammalian/vertebrate_mammalian$num.genomic.gbff.gz; done
-wget -nc ftp://ftp.ncbi.nih.gov/refseq/release/vertebrate_mammalian/vertebrate_mammalian18.genomic.fna.gz;
-wget -nc ftp://ftp.ncbi.nih.gov/refseq/H_sapiens/H_sapiens/Assembled_chromosomes/hs_ref_GRCh37_chr{1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,X,Y}.agp.gz
+for num in `seq 85`; do wget -nc ftp://ftp.ncbi.nih.gov/refseq/release/vertebrate_mammalian/vertebrate_mammalian$num.rna.gbff.gz; done
+#wget -nc ftp://ftp.ncbi.nih.gov/refseq/H_sapiens/H_sapiens/Assembled_chromosomes/hs_ref_GRCh37_chr{1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,X,Y}.agp.gz
 for packed in *.gz; do gunzip $packed; done
