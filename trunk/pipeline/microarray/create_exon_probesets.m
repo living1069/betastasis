c962f7600a061d29fa881dee0432444023265403
matti.annala@tut.fi <matti.annala@tut.fi@3a7f2580-edfc-f23b-ebb8-262a95d2b863>
2011-02-01 07:54:49 +0000

    Exon probeset construction now makes use of the new organism exon model.

--- trunk/pipeline/microarray/create_exon_probesets.m
+++ trunk/pipeline/microarray/create_exon_probesets.m
@@ -1,41 +1,65 @@
 
-% This function constructs exon expression probesets for the current organism,
-% using a set of complementary DNA probes specified by the user.
-%
-% The algorithm is not very smart, and simply places a probe in an exon's
-% probeset if the probe's target sequence or its reverse complement is found
-% in the exon. The biggest issue with this is that some probes will be placed
-% in multiple probesets.
-%
-% Inputs:
-%     probes - Data structure describing a set of complementary DNA probes.
-%     probe_blacklist - A list of probes that should be ignored and not placed
-%	      in any probesets. The probes should be listed by their index.
+% CREATE_EXON_PROBESETS   Builds custom exon expression probesets from probes
 % 
-% Outputs:
-%     probesets - Data structure describing a set of probesets, each targeting
-%         a specific exon of the currently selected organism.
+%    PROBESETS = CREATE_EXON_PROBESETS(PROBES, ...) constructs custom
+%    exon expression probesets from the microarray probes described by the data
+%    structure PROBES. The probesets are designed based on exon annotations
+%    in the currently selected transcriptome build (see HELP ORGANISM).
+%    
+%    The probesets are built by aligning the probe sequences against the
+%    reference exome. A probe is then assigned to the probesets of any exons
+%    whose sequences it aligns against.
 %
+%    CREATE_GENE_PROBESETS(..., 'MaxMismatches', MAX_MM) allows MAX_MM 
+%    nucleotide mismatches when aligning the probes against the exons.
+%    Default is 0 nucleotide mismatches.
+%
+%    CREATE_GENE_PROBESETS(..., 'MinProbesetSize', NPROBES) discards probesets
+%    with less than NPROBES probes. Default is NPROBES = 2.
+
 % Author: Matti Annala <matti.annala@tut.fi>
 
-function probesets = create_exon_probesets(probes)
+function probesets = create_exon_probesets(probes, varargin)
 
 global organism;
 exons = organism.Exons;
 
+min_probes_per_probeset = 2;
+max_mismatches = 0;
+allow_nonspecific_hits = false;
+
+for k = 1:2:length(varargin)
+	if strcmpi(varargin{k}, 'MaxMismatches')
+		max_mismatches = varargin{k+1};
+		continue;
+	end
+	
+	if strcmpi(varargin{k}, 'MinProbesetSize')
+		min_probes_per_probeset = varargin{k+1};
+		continue;
+	end
+	
+	%if strcmpi(varargin{k}, 'AllowNonspecific')
+	%	allow_nonspecific_hits = varargin{k+1};
+	%	continue;
+	%end
+	
+	error('Unrecognized option "%s".', varargin{k});
+end
+
 fprintf(1, 'Writing probe sequences to a temporary file...\n');
-probes_fasta_tmp = ptemp();
+probes_fasta_tmp = ptemp;
 write_seq_fasta(probes, probes_fasta_tmp);
 
-fprintf(1, 'Aligning probes to transcripts using Bowtie...\n');
+fprintf(1, 'Aligning probes to exons using Bowtie...\n');
 [alignments_tmp, ~] = bowtie_align(probes_fasta_tmp, 'exons', ...
-	'-v0 -m10 --suppress 5,6,7,8 ');
+	sprintf('-v%d -m10 -k10 --suppress 5,6,7,8 ', max_mismatches));
 
 fprintf(1, 'Constructing exon probesets based on alignments...\n');
 
-probesets = struct( ...
-	'ProbeCount', zeros(length(exons.Transcript), 1), ...
-	'Probes', zeros(length(exons.Transcript), 4));
+probesets = struct;
+probesets.ProbeCount = zeros(length(exons.ID), 1);
+probesets.Probes = zeros(length(exons.ID), 4);
 
 alignments_file = fopen(alignments_tmp);
 data = textscan(alignments_file, '%d %*s %d %*d');
@@ -54,14 +78,17 @@ for p = 1:length(probe_indices)
 	end
 end
 
-fprintf(1, 'Filtering out probesets with less than three probes...\n');
-probesets.ProbeCount(probesets.ProbeCount < 3) = 0;
-probesets.Probes(probesets.ProbeCount < 3, :) = 0;
+if min_probes_per_probeset > 0
+	fprintf(1, 'Filtering out probesets with less than %d probes...\n', ...
+		min_probes_per_probeset);
+	probesets.ProbeCount(probesets.ProbeCount < min_probes_per_probeset) = 0;
+	probesets.Probes(probesets.ProbeCount < min_probes_per_probeset, :) = 0;
+end
 
 probesets.Type = 'Exon expression';
 probesets.Organism = organism.Name;
-probesets.GenomeVersion = organism.GenomeVersion;
+%probesets.GenomeVersion = organism.GenomeVersion;
 
-system(['rm ' probes_fasta_tmp]);
-system(['rm ' alignments_tmp]);
+safe_delete(probes_fasta_tmp);
+safe_delete(alignments_tmp);
 
