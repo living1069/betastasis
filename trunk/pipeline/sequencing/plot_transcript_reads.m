5e9464393676fef21590f31569632187366eb336
matti.annala@tut.fi <matti.annala@tut.fi@3a7f2580-edfc-f23b-ebb8-262a95d2b863>
2011-05-05 11:03:06 +0000

    plot_transcript_reads() now collects statistics on the number of reads hitting a particular exon or exon junction.

--- trunk/pipeline/sequencing/plot_transcript_reads.m
+++ trunk/pipeline/sequencing/plot_transcript_reads.m
@@ -18,6 +18,7 @@ function stats = plot_transcript_reads(reads, transcript, image_prefix, ...
 	varargin)
 	
 global organism;
+exons = organism.Exons;
 
 if ischar(transcript)
 	tx_idx = transcript_idx(transcript);
@@ -25,22 +26,29 @@ elseif isnumeric(transcript)
 	tx_idx = transcript;
 end
 
-S = length(reads.Raw);
+tx_exons = organism.Transcripts.Exons{tx_idx};
 
 if isfield(reads.Meta.Sample, 'ID')
 	[~, uniq_samples] = unique(reads.Meta.Sample.ID);
 else
-	uniq_samples = 1:S;
+	uniq_samples = 1:length(reads.Raw);
 end
+
+S = length(uniq_samples);
 
 tx_seq = organism.Transcripts.Sequence{tx_idx};
 
-stats.TotalReads = zeros(1, length(uniq_samples));
-stats.AlignedReads = zeros(1, length(uniq_samples));
+stats.Samples = uniq_samples;
+
+stats.TotalReads = zeros(1, S);
+stats.AlignedReads = zeros(1, S);
+
+stats.ExonReads = zeros(length(tx_exons), S);
+stats.JunctionReads = zeros(length(tx_exons) - 1, S);
 
 % We iterate through the samples first, and then we iterate through their
 % technical replicates.
-for s = 1:length(uniq_samples)
+for s = 1:S
 	if isfield(reads.Meta.Sample, 'ID')
 		sample_id = reads.Meta.Sample.ID{uniq_samples(s)};
 	else
@@ -58,6 +66,9 @@ for s = 1:length(uniq_samples)
 	offsets = [];
 	sequences = {};
 	
+	% If we have technical replicates (multiple sequenced channels) from a
+	% particular sample, we combine the alignments from all the replicates and
+	% then plot them together for that sample.
 	for r = replicates'
 		fprintf(1, '-> Channel %s...\n', reads.Meta.Sample.Filename{r});
 		al = align_reads(filter_query(reads, r), 'transcripts', ...
@@ -74,6 +85,37 @@ for s = 1:length(uniq_samples)
 		sequences = cat(1, sequences, al.Sequence(keep));
 	end
 	
+	
+	
+	
+	% Calculate the number of reads aligned to exons and exon junctions.
+	[ordered_offsets, order] = sort(offsets);
+	ex = 1;
+	ex_pos = exons.Position(tx_exons(ex), :);
+	for m = 1:length(ordered_offsets)
+		while 1
+			read_pos = [ordered_offsets(m), ...
+				ordered_offsets(m) + length(sequences{order(m)}) - 1];
+			if read_pos(1) > ex_pos(2)
+				ex = ex + 1;
+				ex_pos = exons.Position(tx_exons(ex), :);
+			elseif read_pos(2) > ex_pos(2)
+				stats.JunctionReads(ex, s) = stats.JunctionReads(ex, s) + 1;
+				break;
+			else
+				stats.ExonReads(ex, s) = stats.ExonReads(ex, s) + 1;
+				break;
+			end
+		end
+	end
+	
+	
+	
+	
+	
+	
+	
+	% Calculate arrow positions.
 	read_count_dist = zeros(1, length(tx_seq));
 	x = zeros(7, length(offsets));
 	y = zeros(7, length(offsets));
@@ -111,10 +153,12 @@ for s = 1:length(uniq_samples)
 %	xlabel('Feature sequence offset'); ylabel('Number of overlapping reads');
 %	saveas(gcf, [image_prefix '_smoothed_' num2str(s) '.pdf']);
 	
-	figure; hold all; xlim([0 length(tx_seq)]); ylim([-9 150]);
+
+
+	% Render the quiver plot.
+	figure; hold all; xlim([0 length(tx_seq)]); ylim([-9 100]);
 	patch(x, y, 'k');
 	
-	tx_exons = organism.Transcripts.Exons{tx_idx};
 	exon_pos = organism.Transcripts.ExonPos{tx_idx};
 	for m = 1:length(tx_exons)
 		left = exon_pos(m, 1);
