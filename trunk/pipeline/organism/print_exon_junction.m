56db6be24380b117daea95a488165b467bc49cc6
matti.annala@tut.fi <matti.annala@tut.fi@3a7f2580-edfc-f23b-ebb8-262a95d2b863>
2011-10-19 19:32:32 +0000

    Fixed some code rot in print_exon_junction().

--- trunk/pipeline/organism/print_exon_junction.m
+++ trunk/pipeline/organism/print_exon_junction.m
@@ -1,15 +1,41 @@
-function [] = print_exon_junction(left_exon, right_exon)
+function [] = print_exon_junction(exon_5p, exon_3p)
 
 global organism;
+genes = organism.Genes;
 transcripts = organism.Transcripts;
 exons = organism.Exons;
 
-left_seq = transcripts.Sequence{exons.Transcript(left_exon)};
-right_seq = transcripts.Sequence{exons.Transcript(right_exon)};
+tokens = regexpi(exon_5p, '(.+)\[(.+)\]', 'tokens');
+if length(tokens) ~= 1, error 'Invalid 5'' exon name.'; end
+token = tokens{1}; gene_5p = token{1}; exon_5p = token{2};
+	
+tokens = regexpi(exon_3p, '(.+)\[(.+)\]', 'tokens');
+if length(tokens) ~= 1, error 'Invalid 3'' exon name.'; end
+token = tokens{1}; gene_3p = token{1}; exon_3p = token{2};
 
-left_seq = left_seq(exons.Position(left_exon, 1):exons.Position(left_exon, 2));
-right_seq = right_seq(exons.Position(right_exon, 1):...
-	exons.Position(right_exon, 2));
+left_gene = gene_idx(gene_5p);
+right_gene = gene_idx(gene_3p);
 
-fprintf(1, '%s|%s\n', upper(left_seq(end-49:end)), upper(right_seq(1:50)));
+if isnan(left_gene), error 'Unknown 5'' gene.'; end
+if isnan(right_gene), error 'Unknown 3'' gene.'; end
+
+exons_5p = [];
+for tx = genes.Transcripts(left_gene, 1:genes.TranscriptCount(left_gene))
+	exons_5p = [exons_5p; transcripts.Exons{tx}];
+end
+exons_3p = [];
+for tx = genes.Transcripts(right_gene, 1:genes.TranscriptCount(right_gene))
+	exons_3p = [exons_3p; transcripts.Exons{tx}];
+end
+
+exons_5p = unique(exons_5p);
+exons_3p = unique(exons_3p);
+
+left_exon = exons_5p(strcmp(exon_5p, exons.ID(exons_5p)));
+right_exon = exons_3p(strcmp(exon_3p, exons.ID(exons_3p)));
+
+left_seq = exons.Sequence{left_exon};
+right_seq = exons.Sequence{right_exon};
+
+fprintf(1, '%s|%s\n', upper(left_seq), upper(right_seq));
 
