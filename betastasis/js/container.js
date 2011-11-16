1c040d7a0809cfd1e25d807e37f82666145ecd0b
hrovira <hrovira@systemsbiology.org>
2011-11-16 22:41:46 +0000

    refactoring container and visualization to implement google visualization api

---
+++ betastasis/js/container.js
@@ -0,0 +1,45 @@
+Ext.ns('org.betastasis.visualizations.mutation_map');
+
+console.log('org.betastasis.visualizations.mutation_map! available at:' + org.betastasis.visualizations.mutation_map);
+
+var MutationMapContainer = Ext.extend(Object, {
+    currentState: {},
+
+    constructor: function(container) {
+        console.log("org.betastasis.visualizations.mutation_map.MutationMapContainer(" + container + ")");
+        Ext.apply(this, {contentEl:container});
+    },
+
+    draw: function(data, options) {
+        console.log("org.betastasis.visualizations.mutation_map.MutationMapContainer(" + data + "," + options + ")");
+
+        var container = Ext.getDom(this.contentEl);
+
+        Ext.Ajax.request({
+            url: data.uri,
+            method: "get",
+            success: function(o) {
+                var data = Ext.util.JSON.decode(o.responseText);
+                var vis = new ProbesetVis(container);
+                vis.draw(data, options);
+            }
+        });
+    },
+
+    GetState: function() {
+        console.log("org.betastasis.visualizations.mutation_map.MutationMapContainer.GetState(): TODO: gather state from contained visualization");
+        return this.currentState;
+    },
+
+    SetState: function(state) {
+        this.currentState = state;
+        console.log("org.betastasis.visualizations.mutation_map.MutationMapContainer.SetState(" + state + "): TODO: pass state on to contained visualization");
+    }
+});
+
+org.betastasis.visualizations.mutation_map.Container = MutationMapContainer;
+
+org.systemsbiology.pages.apis.events.MessageBus.fireEvent("publish", {
+    key: "org.betastasis.visualizations.mutation_map.Container",
+    payload: org.betastasis.visualizations.mutation_map.Container
+});
--- betastasis/js/probeset_vis.js
+++ betastasis/js/probeset_vis.js
@@ -1,6 +1,4 @@
-function ProbesetVis(div) {
-	var w = 600, h = 400;
-	
+function ProbesetVis(container) {
 	var exon_h = 10;
 	var cds_h = 4;
 	
@@ -13,33 +11,11 @@ function ProbesetVis(div) {
 	
 	page = self;
 	self = this;
-	
-	/*
-	function selectRows(d) {
-		var start_row = Math.floor(d.y / h);
-		var end_row = Math.floor((d.y + d.dy) / h);
-		self.dragged_rows = [start_row, end_row];
-		self.drag_bar.render();
-		return false;
-	}
 
-	function endSelectRows(d) {
-		if (d.dy == 0) {
-			var row = Math.floor(d.y / h);
-			self.selected[row] = 1 - self.selected[row];
-		} else if (self.dragged_rows.length == 2) {
-			for (var k = self.dragged_rows[0]; k <= self.dragged_rows[1]; k++) {
-				self.selected[k] = 1 - self.selected[k];
-			}
-		}
-		self.dragged_rows = [];
-		self.selection_bar.render();
-		self.drag_bar.render();
-		return false;
-	}
-	*/
-	
-	this.setGene = function(data) {
+	this.draw = function(data, options) {
+        var w = options.width;
+        var h = options.height;
+
 		this.transcripts = [];
 		this.tx_names = [];
 		tx_label_y = [];
@@ -86,7 +62,8 @@ function ProbesetVis(div) {
 			y_base += highest_level * 5;
 
 			for (var p = 0; p < tx.probe_pos.length; p++) {
-				var probe = tx.probe_pos[p];
+				
+                var probe = tx.probe_pos[p];
 				var max_overlap = 0;
 				for (var s = probe[0]; s <= probe[1]; s++) {
 					if (overlap[s] > max_overlap)
@@ -100,7 +77,7 @@ function ProbesetVis(div) {
 				for (var s = probe[0]; s <= probe[1]; s++) {
 					overlap[s] = level;
 				}
-				
+                
 				var color = 0.0;
 				if (tx.probe_accepted[p] == 0)
 					color = 0.7;
@@ -150,24 +127,17 @@ function ProbesetVis(div) {
 			tx_label_links = ivis.add(pv.Panel);
 			x_axis = ivis.add(pv.Rule);
 			x_ticks = ivis.add(pv.Rule);
-			
-			/*
-			matrix = col_panels.add(pv.Panel);
-						row_labels = vis.add(pv.Label);
-			this.selection_bar = vis.add(pv.Panel);
-			this.drag_bar = vis.add(pv.Panel);
-			*/
 		}
 		
 		this.updateData();
-	}
+	};
 	
 	this.updateData = function() {
-		if (vis == undefined) return;
+		if (container == undefined) return;
 		
 		vis.width(w + 3)
 			.height(tx_label_y[tx_label_y.length-1] + 40)
-			.canvas(div);
+			.canvas(container);
 		
 		ivis.width(w).left(3);
 		
@@ -233,50 +203,7 @@ function ProbesetVis(div) {
 			x.domain(t.x / w * longest_tx, (t.k + t.x / w) * longest_tx);
 			ivis.render();
 		}
-		
-		/*
-		this.selection_bar.data(this.selected)
-			.left(labelsize + this.cols.length * w + 8)
-			.top(function() { return labelsize + this.index * h; })
-			.height(h)
-			.width(3)
-			.fillStyle(function(d) { return d ? 'black':'none'; })
-			.strokeStyle("none")
-			.antialias(false);
-		
-		this.drag_bar.data(this.selected)
-			.left(labelsize + this.cols.length * w + 3)
-			.top(function() { return labelsize + this.index * h; })
-			.height(h)
-			.width(3)
-			.fillStyle(function() {
-				if (self.dragged_rows.length == 2 &&
-					this.index >= self.dragged_rows[0] &&
-					this.index <= self.dragged_rows[1])
-					return '#ccc';
-				return 'none';
-			})
-			.strokeStyle("none")
-			.antialias(false);
-		*/
-		
-		vis.render();
-	}
-	
-	/*
-	this.selectedRows = function() {
-		var ret = '';
-		for (var k = 0; k < this.selected.length; k++) {
-			if (this.selected[k] == 1) {
-				if (ret.length > 0) ret = ret + ', ';
-				ret = ret + this.rows[k];
-			}
-		}
-		if (ret.length == 0) {
-			ret = 'No rows selected.';
-		}
-		return ret;
-	}
-	*/
-}
 
+        vis.render();
+    }
+}
